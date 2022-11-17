450ec508cf0e207e441f03f2dde8ac2037cedcba
Luis Gabriel Janco <janco7249@gmail.com>
2022-11-17 18:48:14 -0400

    feat: add functionality of menu to set and get data of the matrix class

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,30 @@ namespace Matrices
 {
     public partial class Form1 : Form
     {
+        Matrix objMatrix;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objMatrix = new Matrix();
+        }
+
+        private void setMatrixToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int rows, columns, min, max;
+            rows = int.Parse(textBox1.Text);
+            columns = int.Parse(textBox2.Text);
+            min = int.Parse(textBox3.Text);
+            max = int.Parse(textBox4.Text);
+            objMatrix.SetData(rows, columns, min, max);
+        }
+
+        private void getMatrixToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txbMatrix.Text = objMatrix.GetData();
         }
     }
 }
